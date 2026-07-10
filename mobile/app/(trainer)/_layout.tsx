38eefa02292fd0852c28134a774f2ab96d716404
sofisoler <sofisoler_@hotmail.com>
2026-07-09 21:35:10 -0300

    Saco pantallas inexistentes del layout del trainer

--- mobile/app/(trainer)/_layout.tsx
+++ mobile/app/(trainer)/_layout.tsx
@@ -10,45 +10,27 @@ export default function TrainerLayout() {
         headerTitleStyle: { fontWeight: "bold" },
       }}
     >
-      <Stack.Screen
-        name="Home"
-        options={{ title: "Panel del Entrenador" }}
-      />
+      <Stack.Screen name="Home" options={{ title: "Panel del Entrenador" }} />
 
-      <Stack.Screen
-        name="clients/[id]"
-        options={{ title: "Cliente" }}
-      />
+      <Stack.Screen name="clients/[id]" options={{ title: "Cliente" }} />
 
       <Stack.Screen
         name="clients/CrearRutina"
         options={{ title: "Crear Rutina" }}
       />
 
-      <Stack.Screen
-        name="clients/VerRutina"
-        options={{ title: "Rutina" }}
-      />
+      <Stack.Screen name="clients/VerRutina" options={{ title: "Rutina" }} />
 
       <Stack.Screen
         name="clients/CrearBloque"
         options={{ title: "Crear Bloque" }}
       />
 
-      <Stack.Screen
-        name="clients/VerBloque"
-        options={{ title: "Bloque" }}
-      />
+      <Stack.Screen name="clients/VerBloque" options={{ title: "Bloque" }} />
 
-      <Stack.Screen
-        name="clients/CrearDia"
-        options={{ title: "Crear Día" }}
-      />
+      <Stack.Screen name="clients/CrearDia" options={{ title: "Crear Día" }} />
 
-      <Stack.Screen
-        name="clients/DayDetail"
-        options={{ title: "Día" }}
-      />
+      <Stack.Screen name="clients/DayDetail" options={{ title: "Día" }} />
 
       <Stack.Screen
         name="clients/CrearEjercicio"
@@ -56,18 +38,8 @@ export default function TrainerLayout() {
       />
 
       <Stack.Screen
-        name="clients/EditarEjercicio"
-        options={{ title: "Editar Ejercicio" }}
-      />
-
-      <Stack.Screen
         name="clients/EditarProgresion"
         options={{ title: "Editar Progresión" }}
-      />
-
-      <Stack.Screen
-        name="clients/EditarInfoGeneral"
-        options={{ title: "Editar Rutina" }}
       />
     </Stack>
   );
